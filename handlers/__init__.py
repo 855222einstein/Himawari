8c653805a317d8d3ea469e85481c6cabb034f884
855222einstein <shun-bath-backspin@duck.com>
2026-06-16 08:12:44 +0530

    Add files via upload

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -10,6 +10,7 @@ from plugin.lock_system import register_all_lock_plugins
 from plugin.group_guard import register_group_guard
 from plugin.autodelete_system import register_autodelete_plugin
 from plugin.service_delete_system import register_service_delete_plugin
+from plugin.force_sub.force_sub import register_force_sub_plugin
 
 
 def register_all_handlers(app):
@@ -20,5 +21,6 @@ def register_all_handlers(app):
     register_group_guard(app)
     register_autodelete_plugin(app)
     register_service_delete_plugin(app)
+    register_force_sub_plugin(app)
     print("✅ All handlers loaded successfully")
     
