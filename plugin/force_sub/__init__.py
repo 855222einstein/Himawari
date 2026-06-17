dd77aabd57498d7d577ddc9d3b972939680dd362
855222einstein <shun-bath-backspin@duck.com>
2026-06-17 20:12:12 +0530

    Add files via upload

--- plugin/force_sub/__init__.py
+++ plugin/force_sub/__init__.py
@@ -2,11 +2,9 @@
 # plugin/force_sub/__init__.py
 # ============================================================
 
-from plugin.force_sub.force_sub import register_force_sub_plugin as _register
-
-__all__ = ["register_force_sub_plugin"]
+from plugin.force_sub.force_sub import register_force_sub
 
 
 def register_force_sub_plugin(app):
-    _register(app)
-
+    register_force_sub(app)
+    
